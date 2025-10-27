5785b1f2ca0bd1ef0ab6bf2dd3d5393a935c5d5d
mbovero <milesbovero@icloud.com>
2025-10-27 12:37:23 -0600

    first commit

---
+++ src/sleep.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include "config.h"
+
+int main(void)
+{
+    int toggle = 0;
+    stdio_init_all();
+    gpio_init(OUT_PIN);
+    gpio_set_dir(OUT_PIN, GPIO_OUT);
+    gpio_put(OUT_PIN, toggle);
+
+    while (true) {
+        toggle = !toggle;
+        gpio_put(OUT_PIN, toggle);
+        for (int i=0; i < 2000000; i++);
+        sleep_ms(DELAY_MS);
+    }
+}
